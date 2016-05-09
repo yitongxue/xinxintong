57544f7e95eba9a8a25d9efa32e91744792d019e
Jason Young <jasony62@qq.com>
2016-05-10 05:40:58 +0800

    update code

--- cus/db.php
+++ cus/db.php
@@ -1,6 +1,14 @@
 <?php
-define('TMS_MYSQL_HOST', 'localhost');
-define('TMS_MYSQL_PORT', '3306');
-define('TMS_MYSQL_USER', 'root');
-define('TMS_MYSQL_PASS', '');
-define('TMS_MYSQL_DB', 'ytx');
+if (defined('SAE_MYSQL_HOST_M')) {
+	define('TMS_MYSQL_HOST', SAE_MYSQL_HOST_M);
+	define('TMS_MYSQL_PORT', SAE_MYSQL_PORT);
+	define('TMS_MYSQL_USER', SAE_MYSQL_USER);
+	define('TMS_MYSQL_PASS', SAE_MYSQL_PASS);
+	define('TMS_MYSQL_DB', SAE_MYSQL_DB);
+} else {
+	define('TMS_MYSQL_HOST', 'localhost');
+	define('TMS_MYSQL_PORT', '3306');
+	define('TMS_MYSQL_USER', 'root');
+	define('TMS_MYSQL_PASS', '');
+	define('TMS_MYSQL_DB', 'ytx');
+}
