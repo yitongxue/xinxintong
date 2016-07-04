78387244526bebd8a2d1929720eed11e0eeb88e5
Jason Young <jasony62@qq.com>
2016-07-05 07:43:51 +0800

    工作台项目列表打开项目改为用mission_id

--- views/ytx/pl/fe/site/console.js
+++ views/ytx/pl/fe/site/console.js
@@ -22,7 +22,7 @@ ngApp.controller('ctrlMission', ['$scope', 'http2', function($scope, http2) {
         });
     };
     $scope.open = function(mission) {
-        location.href = '/rest/pl/fe/matter/mission?id=' + mission.id + '&site=' + $scope.siteId;
+        location.href = '/rest/pl/fe/matter/mission?id=' + mission.mission_id + '&site=' + $scope.siteId;
     };
     $scope.page = {
         at: 1,
