881006b0ba1160c456177d85f865f1d389294803
Jason Young <jasony62@qq.com>
2016-07-26 23:48:32 +0800

    update code

--- views/ytx/pl/fe/matter/signin/app.js
+++ views/ytx/pl/fe/matter/signin/app.js
@@ -215,23 +215,9 @@ define(['frame', 'schema', 'wrap'], function(ngApp, schemaLib, wrapLib) {
 				$scope.$broadcast('xxt.matter.signin.app.data_schemas.created', newSchema);
 			});
 		};
-		$scope.newMember = function() {
-			if (!$scope.memberSchemas || $scope.memberSchemas.length === 0) return;
-
-			var memberSchema = $scope.memberSchemas[0],
-				newSchema = schemaLib.newInput('member');
-
-			newSchema.schema_id = memberSchema.id;
-			if (memberSchema.attr_name[0] === '0') {
-				newSchema.title = '姓名';
-				newSchema.id = 'member.name';
-			} else if (memberSchema.attr_mobile[0] === '0') {
-				newSchema.title = '手机';
-				newSchema.id = 'member.mobile';
-			} else if (memberSchema.attr_email[0] === '0') {
-				newSchema.title = '邮箱';
-				newSchema.id = 'member.email';
-			}
+		$scope.copySchema = function(schema) {
+			var newSchema = angular.copy(schema);
+			newSchema.id = 'c' + (new Date() * 1);
 			$scope.app.data_schemas.push(newSchema);
 			$scope.update('data_schemas').then(function() {
 				$scope.$broadcast('xxt.matter.signin.app.data_schemas.created', newSchema);
@@ -431,46 +417,16 @@ define(['frame', 'schema', 'wrap'], function(ngApp, schemaLib, wrapLib) {
 				$scope.$broadcast('xxt.editable.add', newOp);
 			});
 		};
+		$scope.onKeyup = function(event) {
+			// 回车时自动添加选项
+			if (event.keyCode === 13) {
+				$scope.addOption();
+			}
+		};
 		$scope.$on('xxt.editable.remove', function(e, op) {
 			var i = $scope.schema.ops.indexOf(op);
 			$scope.schema.ops.splice(i, 1);
 		});
-		$scope.shiftMemberSchema = function() {
-			var memberSchema = $scope.selectedMemberSchema.schema,
-				schemaAttrs = [];
-			$scope.schema.schema_id = memberSchema.id;
-			/*自定义用户属性列表*/
-			memberSchema.attr_name[0] === '0' && (schemaAttrs.push({
-				id: 'name',
-				label: '姓名'
-			}));
-			memberSchema.attr_mobile[0] === '0' && (schemaAttrs.push({
-				id: 'mobile',
-				label: '手机'
-			}));
-			memberSchema.attr_email[0] === '0' && (schemaAttrs.push({
-				id: 'email',
-				label: '邮箱'
-			}));
-			if (memberSchema.extattr && memberSchema.extattr.length) {
-				var i, l, ea;
-				for (i = 0, l = memberSchema.extattr.length; i < l; i++) {
-					ea = memberSchema.extattr[i];
-					schemaAttrs.push({
-						id: 'extattr.' + ea.id,
-						label: ea.label
-					});
-				}
-			}
-			$scope.selectedMemberSchema.attrs = schemaAttrs;
-			$scope.selectedMemberSchema.attr = null;
-		};
-		$scope.shiftMemberSchemaAttr = function() {
-			var attr = $scope.selectedMemberSchema.attr;
-			selectedMemberSchema = attr.label;
-			$scope.schema.id = 'member.' + attr.id;
-			$scope.schema.title = attr.label;
-		};
 		$scope.$watch('schema.ops', function(nv, ov) {
 			if (nv !== ov) {
 				$scope.updWrap('schema', 'ops');
@@ -491,6 +447,7 @@ define(['frame', 'schema', 'wrap'], function(ngApp, schemaLib, wrapLib) {
 				/* 更新应用的定义 */
 				$scope.update('data_schemas').then(function() {
 					/* 更新当前页面 */
+					$scope.ep.purifyInput(tinymce.activeEditor.getContent(), true);
 					$scope.updPage($scope.ep, ['data_schemas', 'html']);
 					/* 更新其它页面 */
 					angular.forEach($scope.app.pages, function(page) {
@@ -505,34 +462,6 @@ define(['frame', 'schema', 'wrap'], function(ngApp, schemaLib, wrapLib) {
 				timerOfUpdate = null;
 			});
 		};
-		if ($scope.schema.type === 'member') {
-			if ($scope.schema.schema_id) {
-				/*自定义用户*/
-				for (var i = $scope.memberSchemas.length - 1; i >= 0; i--) {
-					if ($scope.schema.schema_id === $scope.memberSchemas[i].id) {
-						$scope.selectedMemberSchema = {
-							schema: $scope.memberSchemas[i]
-						};
-						break;
-					}
-				}
-				$scope.selectedMemberSchema.schema && $scope.shiftMemberSchema();
-				/*自定义用户属性*/
-				var id = $scope.schema.id.substr(7);
-				for (var i = $scope.selectedMemberSchema.attrs.length - 1; i >= 0; i--) {
-					if (id === $scope.selectedMemberSchema.attrs[i].id) {
-						$scope.selectedMemberSchema.attr = $scope.selectedMemberSchema.attrs[i];
-						break;
-					}
-				}
-			} else {
-				$scope.selectedMemberSchema = {
-					schema: null,
-					attrs: null,
-					attr: null
-				};
-			}
-		}
 	}]);
 	/**
 	 * value wrap controller
@@ -665,6 +594,7 @@ define(['frame', 'schema', 'wrap'], function(ngApp, schemaLib, wrapLib) {
 				$timeout.cancel(timerOfUpdate);
 			}
 			timerOfUpdate = $timeout(function() {
+				$scope.ep.purifyInput(tinymce.activeEditor.getContent(), true);
 				$scope.updPage($scope.ep, ['act_schemas', 'html']);
 			}, 1000);
 			timerOfUpdate.then(function() {
@@ -805,7 +735,7 @@ define(['frame', 'schema', 'wrap'], function(ngApp, schemaLib, wrapLib) {
 		};
 		$scope.moveWrap = function(action) {
 			$scope.activeWrap = $scope.ep.moveWrap(action);
-			tinymceEditor.save();
+			$scope.updPage($scope.ep, ['html']);
 		};
 		$scope.embedMatter = function(page) {
 			var options = {
