8fc141996355ffd9a6a586addbd638ff0983d075
Jason Young <jasony62@qq.com>
2016-08-12 11:44:49 +0800

    update code

--- controllers/pl/fe/upgrade2.php
+++ controllers/pl/fe/upgrade2.php
@@ -23,37 +23,37 @@ class upgrade2 extends \TMS_CONTROLLER {
 			$sql = "aid='57a48aec7f546' and state=1";
 			$sql .= " and data like '%\"c1470401942360\":\"" . $record->a . "\"%'";
 			$sql .= " and data like '%\"phase\":\"57a48917ae900\"%'";
-			$cnt = $model->query_val_ss(['count(*)'], 'xxt_signin_record', $sql);
+			$cnt = $model->query_val_ss(['count(*)', 'xxt_signin_record', $sql]);
 			$model->update('tmp_stat', ['c' => $cnd]);
 
 			$sql = "aid='57a48aec7f546' and state=1";
 			$sql .= " and data like '%\"c1470401942360\":\"" . $record->a . "\"%'";
 			$sql .= " and data like '%\"phase\":\"57a489181db09\"%'";
-			$cnt = $model->query_val_ss(['count(*)'], 'xxt_signin_record', $sql);
+			$cnt = $model->query_val_ss(['count(*)', 'xxt_signin_record', $sql]);
 			$model->update('tmp_stat', ['d' => $cnd]);
 
 			$sql = "aid='57a48aec7f546' and state=1";
 			$sql .= " and data like '%\"c1470401942360\":\"" . $record->a . "\"%'";
 			$sql .= " and data like '%\"phase\":\"57a489188065e\"%'";
-			$cnt = $model->query_val_ss(['count(*)'], 'xxt_signin_record', $sql);
+			$cnt = $model->query_val_ss(['count(*)'] 'xxt_signin_record', $sql]);
 			$model->update('tmp_stat', ['e' => $cnd]);
 
 			$sql = "aid='57a48aec7f546' and state=1";
 			$sql .= " and data like '%\"c1470401942360\":\"" . $record->a . "\"%'";
 			$sql .= " and data like '%\"phase\":\"57a48918e4ade\"%'";
-			$cnt = $model->query_val_ss(['count(*)'], 'xxt_signin_record', $sql);
+			$cnt = $model->query_val_ss(['count(*)', 'xxt_signin_record', $sql]);
 			$model->update('tmp_stat', ['f' => $cnd]);
 
 			$sql = "aid='57a48aec7f546' and state=1";
 			$sql .= " and data like '%\"c1470401942360\":\"" . $record->a . "\"%'";
 			$sql .= " and data like '%\"phase\":\"57a4891954e4c\"%'";
-			$cnt = $model->query_val_ss(['count(*)'], 'xxt_signin_record', $sql);
+			$cnt = $model->query_val_ss(['count(*)', 'xxt_signin_record', $sql]);
 			$model->update('tmp_stat', ['g' => $cnd]);
 
 			$sql = "aid='57a48aec7f546' and state=1";
 			$sql .= " and data like '%\"c1470401942360\":\"" . $record->a . "\"%'";
 			$sql .= " and data like '%\"phase\":\"57a48919b4bb9\"%'";
-			$cnt = $model->query_val_ss(['count(*)'], 'xxt_signin_record', $sql);
+			$cnt = $model->query_val_ss(['count(*)', 'xxt_signin_record', $sql]);
 			$model->update('tmp_stat', ['h' => $cnd]);
 
 		}
