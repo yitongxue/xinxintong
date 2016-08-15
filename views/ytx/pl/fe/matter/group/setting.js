c035507fef1c6a09bdb121b5750f129758f943e3
Jason Young <jasony62@qq.com>
2016-08-15 10:04:33 +0800

    update code

--- views/ytx/pl/fe/matter/group/setting.js
+++ views/ytx/pl/fe/matter/group/setting.js
@@ -14,6 +14,7 @@ define(['frame'], function(ngApp) {
 				return message;
 			}
 		};
+		$scope.activeRound = null;
 		$scope.assignMission = function() {
 			mattersgallery.open($scope.siteId, function(matters, type) {
 				var app;
@@ -87,6 +88,7 @@ define(['frame'], function(ngApp) {
 					data.appType === 'signin' && (params.includeEnroll = data.includeEnroll);
 					http2.post('/rest/pl/fe/matter/group/player/importByApp?site=' + $scope.siteId + '&app=' + $scope.id, params, function(rsp) {
 						$scope.app.sourceApp = data.app;
+						$scope.open(null);
 					});
 				}
 			});
@@ -208,9 +210,8 @@ define(['frame'], function(ngApp) {
 				});
 			}
 		};
-		$scope.editingRound = null;
 		$scope.open = function(round) {
-			$scope.editingRound = round;
+			$scope.activeRound = round;
 		};
 		$scope.value2Label = function(val, key) {
 			var schemas = $scope.app.data_schemas,
@@ -233,16 +234,16 @@ define(['frame'], function(ngApp) {
 		};
 		$scope.updateRound = function(name) {
 			var nv = {};
-			nv[name] = $scope.editingRound[name];
-			http2.post('/rest/pl/fe/matter/group/round/update?site=' + $scope.siteId + '&app=' + $scope.id + '&rid=' + $scope.editingRound.round_id, nv, function(rsp) {
+			nv[name] = $scope.activeRound[name];
+			http2.post('/rest/pl/fe/matter/group/round/update?site=' + $scope.siteId + '&app=' + $scope.id + '&rid=' + $scope.activeRound.round_id, nv, function(rsp) {
 				noticebox.success('完成保存');
 			});
 		};
 		$scope.removeRound = function() {
-			http2.get('/rest/pl/fe/matter/group/round/remove?site=' + $scope.siteId + '&app=' + $scope.id + '&rid=' + $scope.editingRound.round_id, function(rsp) {
-				var i = $scope.rounds.indexOf($scope.editingRound);
+			http2.get('/rest/pl/fe/matter/group/round/remove?site=' + $scope.siteId + '&app=' + $scope.id + '&rid=' + $scope.activeRound.round_id, function(rsp) {
+				var i = $scope.rounds.indexOf($scope.activeRound);
 				$scope.rounds.splice(i, 1);
-				$scope.editingRound = null;
+				$scope.activeRound = null;
 			});
 		};
 		$scope.export = function() {
@@ -305,11 +306,11 @@ define(['frame'], function(ngApp) {
 			return labels.join(',');
 		};
 		$scope.saveTargets = function() {
-			$scope.editingRound.targets = $scope.aTargets;
+			$scope.activeRound.targets = $scope.aTargets;
 			$scope.updateRound('targets');
 		};
 	}]);
-	ngApp.provider.controller('ctrlRunning', ['$scope', '$uibModal', 'http2', function($scope, $uibModal, http2) {
+	ngApp.provider.controller('ctrlPlayers', ['$scope', '$uibModal', 'http2', function($scope, $uibModal, http2) {
 		$scope.editPlayer = function(player) {
 			$uibModal.open({
 				templateUrl: 'editorPlayer.html',
@@ -329,8 +330,6 @@ define(['frame'], function(ngApp) {
 			}).result.then(function(updated) {
 				var p = updated[0];
 				http2.post('/rest/pl/fe/matter/group/player/update?site=' + $scope.siteId + '&app=' + $scope.id + '&ek=' + player.enroll_key, p, function(rsp) {
-					//tags = updated[1];
-					//$scope.app.tags = tags;
 					angular.extend(player, rsp.data);
 				});
 			});
@@ -360,28 +359,107 @@ define(['frame'], function(ngApp) {
 				});
 			});
 		};
-		$scope.removePlayer = function(record) {
+		$scope.removePlayer = function(player) {
 			if (window.confirm('确认删除？')) {
-				http2.get('/rest/pl/fe/matter/group/player/remove?site=' + $scope.siteId + '&app=' + $scope.id + '&ek=' + record.enroll_key, function(rsp) {
-					var i = $scope.players.indexOf(record);
+				http2.get('/rest/pl/fe/matter/group/player/remove?site=' + $scope.siteId + '&app=' + $scope.id + '&ek=' + player.enroll_key, function(rsp) {
+					var i = $scope.players.indexOf(player);
 					$scope.players.splice(i, 1);
 					$scope.page.total = $scope.page.total - 1;
 				});
 			}
 		};
+		// 当前选中的行
+		$scope.rows = {
+			allSelected: 'N',
+			selected: {}
+		};
+		$scope.selectedPlayers = [];
+		$scope.selectPlayer = function(player) {
+			if ($scope.selectedPlayers.indexOf(player) === -1) {
+				$scope.selectedPlayers.push(player);
+			} else {
+				$scope.selectedPlayers.splice($scope.selectedPlayers.indexOf(player), 1);
+			}
+		};
+		// 选中或取消选中所有行
+		$scope.selectAllRows = function(checked) {
+			var index = 0;
+			if (checked === 'Y') {
+				$scope.selectedPlayers = [];
+				while (index < $scope.players.length) {
+					$scope.selectedPlayers.push($scope.players[index]);
+					$scope.rows.selected[index++] = true;
+				}
+			} else if (checked === 'N') {
+				$scope.rows.selected = {};
+				$scope.selectedPlayers = [];
+			}
+		};
+		/**
+		 * 选中的用户移出分组
+		 */
+		$scope.quitGroup = function(players) {
+			if ($scope.activeRound && players.length) {
+				var url, eks = [];
+
+				url = '/rest/pl/fe/matter/group/player/quitGroup?site=' + $scope.siteId + '&app=' + $scope.id;
+				url += '&round=' + $scope.activeRound.round_id;
+
+				angular.forEach(players, function(player) {
+					eks.push(player.enroll_key);
+				});
+
+				http2.post(url, eks, function(rsp) {
+					var result = rsp.data;
+					angular.forEach(players, function(player) {
+						if (result[player.enroll_key] !== false) {
+							$scope.players.splice($scope.players.indexOf(player), 1);
+						}
+					});
+					$scope.rows.allSelected = 'N';
+					$scope.rows.selected = {};
+					$scope.selectedPlayers = [];
+				});
+			}
+		};
+		/**
+		 * 选中的用户移入分组
+		 */
+		$scope.joinGroup = function(round, players) {
+			if (round && players.length) {
+				var url, eks = [];
+
+				url = '/rest/pl/fe/matter/group/player/joinGroup?site=' + $scope.siteId + '&app=' + $scope.id;
+				url += '&round=' + round.round_id;
+
+				angular.forEach(players, function(player) {
+					eks.push(player.enroll_key);
+				});
+
+				http2.post(url, eks, function(rsp) {
+					var result = rsp.data;
+					angular.forEach(players, function(player) {
+						if (result[player.enroll_key] !== false) {
+							if ($scope.activeRound === false) {
+								$scope.players.splice($scope.players.indexOf(player), 1);
+							} else if ($scope.activeRound === null) {
+								player.round_id = round.round_id;
+								player.round_title = round.title;
+							}
+						}
+					});
+					$scope.rows.allSelected = 'N';
+					$scope.rows.selected = {};
+					$scope.selectedPlayers = [];
+				});
+			}
+		};
 		$scope.empty = function() {
 			var vcode;
-			vcode = prompt('是否要删除所有登记信息？，若是，请输入活动名称。');
+			vcode = prompt('是否要从【' + $scope.app.title + '】删除所有用户？，若是，请输入活动名称。');
 			if (vcode === $scope.app.title) {
 				http2.get('/rest/pl/fe/matter/group/player/empty?site=' + $scope.siteId + '&app=' + $scope.id, function(rsp) {
-					var round = $scope.editingRound;
-					if (round === null) {
-						$scope.allPlayers();
-					} else if (round === false) {
-						$scope.pendings();
-					} else {
-						$scope.winners(round);
-					}
+					$scope.doSearch(1);
 				});
 			}
 		};
@@ -405,18 +483,35 @@ define(['frame'], function(ngApp) {
 				$scope.players = rsp.data;
 			});
 		};
+		/**
+		 * 完成用户同步操作
+		 */
 		$scope.$on('xxt.matter.group.player.sync', function(event, count) {
 			if (count > 0) {
-				if ($scope.editingRound === null) {
+				if ($scope.activeRound === null) {
 					$scope.allPlayers();
-				} else if ($scope.editingRound === false) {
+				} else if ($scope.activeRound === false) {
 					$scope.pendings();
 				} else {
-					$scope.winners($scope.editingRound);
+					$scope.winners($scope.activeRound);
 				}
 			}
 		});
-		$scope.$watch('editingRound', function(round) {
+		/**
+		 * 完成自动分组操作
+		 */
+		$scope.$on('xxt.matter.group.execute.done', function(winners) {
+			if ($scope.activeRound === null) {
+				$scope.allPlayers();
+			} else if ($scope.activeRound === false) {
+				$scope.pendings();
+			} else {
+				$scope.winners($scope.activeRound);
+			}
+		});
+		// 表格定义是否准备完毕
+		$scope.tableReady = 'N';
+		$scope.$watch('activeRound', function(round) {
 			if (round === null) {
 				$scope.allPlayers();
 			} else if (round === false) {
@@ -424,15 +519,7 @@ define(['frame'], function(ngApp) {
 			} else {
 				$scope.winners(round);
 			}
-		});
-		$scope.$on('xxt.matter.group.execute.done', function(winners) {
-			if ($scope.editingRound === null) {
-				$scope.allPlayers();
-			} else if ($scope.editingRound === false) {
-				$scope.pendings();
-			} else {
-				$scope.winners($scope.editingRound);
-			}
+			$scope.tableReady = 'Y';
 		});
 	}]);
 });
