e9c6b944a8d40510dbc53ba9c6967319311c6ac4
Jason Young <jasony62@qq.com>
2016-12-15 15:48:30 +0800

    update code

--- views/ytx/pl/fe/matter/enroll/frame.js
+++ views/ytx/pl/fe/matter/enroll/frame.js
@@ -3,7 +3,7 @@ define(['require', 'page', 'schema'], function(require, pageLib, schemaLib) {
 	var ngApp = angular.module('app', ['ngRoute', 'frapontillo.bootstrap-switch', 'ui.tms', 'tmplshop.ui.xxt', 'service.matter', 'service.enroll', 'tinymce.enroll', 'ui.xxt', 'channel.fe.pl']);
 	ngApp.config(['$controllerProvider', '$routeProvider', '$locationProvider', '$compileProvider', '$uibTooltipProvider', 'srvQuickEntryProvider', 'srvAppProvider', 'srvPageProvider', 'srvRecordProvider', function($controllerProvider, $routeProvider, $locationProvider, $compileProvider, $uibTooltipProvider, srvQuickEntryProvider, srvAppProvider, srvPageProvider, srvRecordProvider) {
 		var RouteParam = function(name, baseURL) {
-			!baseURL && (baseURL = '/views/default/pl/fe/matter/enroll/');
+			!baseURL && (baseURL = '/views/ytx/pl/fe/matter/enroll/');
 			this.templateUrl = baseURL + name + '.html?_=' + (new Date() * 1);
 			this.controller = 'ctrl' + name[0].toUpperCase() + name.substr(1);
 			this.resolve = {
