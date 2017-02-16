d8f30875efad1a6ee86363140119f43c913a4451
Jason Young <jasony62@qq.com>
2017-02-16 16:25:45 +0800

    update code

--- views/ytx/pl/fe/matter/article/frame.js
+++ views/ytx/pl/fe/matter/article/frame.js
@@ -76,13 +76,11 @@ define(['require'], function() {
             $scope.site = oSite;
         });
         srvApp.get().then(function(editing) {
-            var url;
             $scope.editing = editing;
             !editing.attachments && (editing.attachments = []);
-            url = 'http://' + location.host + '/rest/site/fe/matter?site=' + $scope.editing.siteid + '&id=' + $scope.editing.id + '&type=article';
             $scope.entry = {
-                url: url,
-                qrcode: '/rest/site/fe/matter/article/qrcode?site=' + $scope.editing.siteid + '&url=' + encodeURIComponent(url),
+                url: editing.entryUrl,
+                qrcode: '/rest/site/fe/matter/article/qrcode?site=' + $scope.editing.siteid + '&url=' + encodeURIComponent(editing.entryUrl),
             };
             // 用户评论
             if (editing.can_discuss === 'Y') {
@@ -92,7 +90,7 @@ define(['require'], function() {
                     domain: editing.siteid
                 };
             }
-        })
+        });
     }]);
     /***/
     require(['domReady!'], function(document) {
