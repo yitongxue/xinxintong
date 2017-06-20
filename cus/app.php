dce66301a7d88b99c54a33f5e3f652cd0198f3ff
Jason Young <jasony62@qq.com>
2017-06-20 15:08:21 +0800

    update code

--- cus/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 应用的标题 */
-define('APP_TITLE', '易同学');
-/* 应用程序视图名称，起始路径为：TMS_APP_DIR.'/views/'.TMS_APP_VIEW_NAME */
-define('TMS_APP_VIEW_NAME', 'ytx');
-/* 应用程序模版起始目录 */
-define('TMS_APP_TEMPLATE', dirname(__FILE__) . '/_template');
-/* 缺省页 */
-!defined('TMS_APP_HOME') && define('TMS_APP_HOME', '/rest/pl/fe');
