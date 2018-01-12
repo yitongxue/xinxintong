38b1b2a380415918d04f873f6e1ef541c479b240
Jason Young <jasony62@qq.com>
2018-01-12 16:42:01 +0800

    update code

--- models/sns/reply/mschema.php
+++ models/sns/reply/mschema.php
@@ -17,21 +17,21 @@ class mschema_model extends MultiArticleReply {
 		$oCard->title = $oMschema->title;
 		$oCard->entryURL = $url;
 
-		// switch ($oMschema->matter_type) {
-		// case 'mission':
-		// 	if (!empty($oMschema->matter_id)) {
-		// 		$oMission = \TMS_APP::->model('matter\mission')->byId($oMschema->matter_id, ['fields' => 'title,pic']);
-		// 		if ($oMission) {
-		// 			$oCard->summary = $oMission->title . '需要您填写【' . $oMschema->title . '】信息。';
-		// 			$oCard->pic = $oMission->pic;
-		// 		}
-		// 	}
-		// 	break;
-		// default:
-		$oSite = \TMS_APP::model('site')->byId($oMschema->siteid, ['fields' => 'id,name,heading_pic']);
-		$oCard->summary = $oSite->name . '邀请您填写【' . $oMschema->title . '】信息。';
-		$oCard->pic = $oSite->heading_pic;
-		//}
+		switch ($oMschema->matter_type) {
+		case 'mission':
+			if (!empty($oMschema->matter_id)) {
+				$oMission = \TMS_APP::model('matter\mission')->byId($oMschema->matter_id, ['fields' => 'title,pic']);
+				if ($oMission) {
+					$oCard->summary = $oMission->title . '需要您填写【' . $oMschema->title . '】信息。';
+					$oCard->pic = $oMission->pic;
+				}
+			}
+			break;
+		default:
+			$oSite = \TMS_APP::model('site')->byId($oMschema->siteid, ['fields' => 'id,name,heading_pic']);
+			$oCard->summary = $oSite->name . '邀请您填写【' . $oMschema->title . '】信息。';
+			$oCard->pic = $oSite->heading_pic;
+		}
 
 		return [$oCard];
 	}
