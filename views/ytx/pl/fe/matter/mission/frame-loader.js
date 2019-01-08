8511e6df3f20e3a7c8f9a027a343caadc68f7e31
Jason Young <jasony62@qq.com>
2019-01-08 22:48:59 +0800

    update code

--- views/ytx/pl/fe/matter/mission/frame-loader.js
+++ views/ytx/pl/fe/matter/mission/frame-loader.js
@@ -37,6 +37,7 @@ requirejs(['/static/js/tms.bootstrap.js'], function(tms) {
             "overview": '/views/default/pl/fe/matter/mission/overview',
             "report": '/views/default/pl/fe/matter/mission/report',
             "time": '/views/default/pl/fe/matter/mission/time',
+            "pagelet": '/views/default/pl/fe/matter/mission/pagelet',
             "timerNotice": '/views/default/pl/fe/_module/timerNotice',
             "entryRule": '/views/default/pl/fe/_module/entryRule',
         }
